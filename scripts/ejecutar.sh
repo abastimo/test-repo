a8136f1c09975fb9d68f9690741ea9e2aeab00ed
abastimo <abastimo@emeal.nttdata.com>
2026-09-21 11:49:19 -0500

    add log

--- scripts/ejecutar.sh
+++ scripts/ejecutar.sh
@@ -8,16 +8,19 @@ PID1=$!
 bash scripts/hola2.txt &
 PID2=$!
 
-wait $PID1
+echo "PID Hola1: $PID1"
+echo "PID Hola2: $PID2"
+
+wait "$PID1"
 STATUS1=$?
 
-wait $PID2
+wait "$PID2"
 STATUS2=$?
 
 echo "Finalizaron ambos procesos."
 
-if [ $STATUS1 -ne 0 ] || [ $STATUS2 -ne 0 ]; then
-    echo "Alguno de los procesos falló."
+if [ "$STATUS1" -ne 0 ] || [ "$STATUS2" -ne 0 ]; then
+    echo "Alguno de los procesos fallo."
     exit 1
 fi
 
